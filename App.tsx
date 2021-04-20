64b8e0e161b7985f9d302cf8ccb9fdd79ee659ed
Vlad <vladik280699@gmail.com>
2021-04-20 17:46:15 +0300

    add eslint

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+ const App: React.FC = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -17,3 +17,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App
